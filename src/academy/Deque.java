802fb5ea3329e23ec9cd57ea44b711b30cd5a0f7
a1lexen <alex3003000@gmail.com>
2018-10-18 00:06:08 +0300

    Added new methods like remove() and sort()

--- src/academy/Deque.java
+++ src/academy/Deque.java
@@ -2,25 +2,32 @@ package academy;
 
 import java.util.*;
 
-class Deque <Item> {
+class Deque <Item extends Comparable<? super Item>> {
     private Item[] values;
     private int size;
-    private int itemCount, firstPos, lastPos;
+    public int itemCount, firstPos, lastPos;
     private final int MIN_SIZE = 2;
 
     public Deque() {
-        values = (Item[]) new Object[MIN_SIZE];
+        values = (Item[]) new Comparable[MIN_SIZE];
         size = MIN_SIZE;
         itemCount = 0;
         firstPos = 0;
         lastPos = 0;
     }
 
-    public boolean isEmpty() {
-        return size() == 0;
-    }
+    public boolean isEmpty() { return size() == 0; }
+
     public int size() {
         return itemCount;
+    }
+
+    public void clear() {
+        values = (Item[]) new Comparable[MIN_SIZE];
+        size = MIN_SIZE;
+        itemCount = 0;
+        firstPos = 0;
+        lastPos = 0;
     }
 
     public void pushFront(Item data) {
@@ -55,18 +62,18 @@ class Deque <Item> {
         }
     }
 
-    public Item popLast() {
+    public Item popBack() {
         Item data;
         if(isEmpty())
             throw new NoSuchElementException("Deque's empty!");
+        lastPos--;
         data = values[lastPos];
         values[lastPos] = null;
-        lastPos--;
         itemCount--;
         return data;
     }
 
-    public Item popFirst() {
+    public Item popFront() {
         Item data;
         if(isEmpty())
             throw new NoSuchElementException("Deque's empty!");
@@ -85,6 +92,38 @@ class Deque <Item> {
         return list;
     }
 
+    public void sort() {
+        ArrayList<Item> arr = asList();
+        Collections.sort(arr);
+        for(int i = 0, j = firstPos; i < arr.size(); i++) {
+            values[j++] = arr.get(i);
+        }
+    }
+
+    public void remove(int begin, int end) {
+        ArrayList<Item> arr = asList();     //We can use vanilla arrays as well, but then we should write our own Array.addAll() method.
+        if (end >= lastPos - firstPos)
+            throw new IndexOutOfBoundsException("End index is too large");
+        else if (begin < 0)
+            throw new IndexOutOfBoundsException("Begin index is too small");
+        else  {
+            for(int i = 0; i < values.length; i++) {
+                values[i] = null;
+            }
+            //newArr = arr[firstPos; begin) + arr(end; lastPos]
+            ArrayList<Item>  newArr = new ArrayList<Item>();
+            newArr.addAll(arr.subList(0, begin));
+            //System.out.println(newArr.toString());
+            newArr.addAll(arr.subList(end + 1, lastPos - firstPos));
+            //System.out.println(newArr.toString());
+            lastPos = firstPos + newArr.size();
+            for(int i = 0, j = firstPos; i < newArr.size(); i++) {
+                values[j++] = newArr.get(i);
+            }
+            itemCount -= (end - begin + 1); //+1 because of having value of index, that is remove(0,4) deletes only 4 items when we need 5
+        }
+    }
+
     @Override
     public String toString() {
         StringBuilder str = new StringBuilder();
@@ -97,7 +136,7 @@ class Deque <Item> {
     private void resize(int currentSize) {
         int newSize = currentSize * currentSize;
         int start = (currentSize * (currentSize - 1)) / 2 ;
-        Item[] newArr = (Item[]) new Object[newSize];
+        Item[] newArr = (Item[]) new Comparable[newSize];
         System.arraycopy(values, firstPos, newArr, start, itemCount);
         size = newSize;
         firstPos = start;
@@ -105,23 +144,58 @@ class Deque <Item> {
         values = newArr;
     }
 
+
     public static void main(String[] args) {
         Deque<Integer> deq = new Deque<>();
+        System.out.println("--TEST OF 'REMOVE' METHOD--");
+        deq.pushBack(6);
+        deq.pushBack(7);
+        deq.pushBack(8);
+        deq.pushBack(9);
+        deq.pushFront(3);
+        System.out.println(deq.toString());
+
+        deq.remove(0, 4);
+        System.out.println(deq.toString());
+        System.out.println(deq.itemCount);
 
         deq.pushBack(6);
         deq.pushBack(7);
-        deq.pushBack(7);
-        deq.pushBack(7);
+        deq.pushBack(11);
+        deq.pushBack(13);
+        System.out.println(deq.toString());
+
+        deq.remove(1, 2);
+        System.out.println(deq.toString());
+        deq.clear();
+
+        System.out.println("--TEST OF 'POP' METHOD--");
+        deq.pushBack(6);
         deq.pushBack(7);
         deq.pushBack(8);
+        deq.pushBack(9);
+        deq.pushFront(3);
+        System.out.println(deq.asList());
 
-        System.out.println(deq.toString());
-        Integer a = deq.popFirst();
-        a = deq.popLast();
-        System.out.println(deq.toString());
+        deq.popBack();
+        System.out.println(deq);
+        deq.popBack();
+        System.out.println(deq);
+        deq.pushBack(8);
+        deq.pushFront(2);
+        System.out.println(deq);
+        deq.remove(0, 3);
+        System.out.println(deq);
 
-        ArrayList<Integer> arrayList = deq.asList();
-        System.out.println(arrayList.toString());
+        deq.pushFront(2);
+        deq.pushFront(2);
+        deq.pushFront(2);
+        deq.pushFront(2);
+        deq.pushFront(3);
+        System.out.println(deq.asList());
+        deq.sort();
+        System.out.println(deq.asList());
+
     }
 
 }
